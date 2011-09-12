39345caa7d5a65b915734f8ddc2ce9e9c4def5fe
Aaron Carlino <unclecheese@leftandmain.com>
2011-09-12 11:45:49 -0400

    MINOR: rename lang file

---
+++ lang/de_DE.php
@@ -0,0 +1,41 @@
+<?php
+global $lang;
+
+$lang['de_DE']['DataObjectManager']['ADD'] = 'Hinzufügen:';
+$lang['de_DE']['DataObjectManager']['ADDEDNEW'] = '%s erfolgreich hinzugefügt';
+$lang['de_DE']['DataObjectManager']['ADDITEM'] = array(
+	'%s hinzufügen',
+	PR_MEDIUM,
+	'Hinzufügen: [name]'
+);
+$lang['de_DE']['DataObjectManager']['ALLOWEDFILETYPES'] = 'Es sind nur %s Dateien erlaubt.';
+$lang['de_DE']['DataObjectManager']['DELETE'] = 'Löschen';
+$lang['de_DE']['DataObjectManager']['DISPLAYING'] = 'Zeige';
+$lang['de_DE']['DataObjectManager']['DRAGDROP'] = 'Erlaube Drag & Drop neuordnen';
+$lang['de_DE']['DataObjectManager']['ERROR'] = 'Fehler';
+$lang['de_DE']['DataObjectManager']['IMPORTFROMFOLDER'] = 'Importieren aus einem existierenden Ordner';
+$lang['de_DE']['DataObjectManager']['NOFILTER'] = 'Kein Filter';
+$lang['de_DE']['DataObjectManager']['NOITEMSFOUND'] = 'Keine %s gefunden';
+$lang['de_DE']['DataObjectManager']['OF'] = 'von';
+$lang['de_DE']['DataObjectManager']['PERPAGERESULTS'] = 'Resultate pro Seite';
+$lang['de_DE']['DataObjectManager']['PERPAGESHOW'] = 'zeige';
+$lang['de_DE']['DataObjectManager']['REFRESH'] = 'aktualisieren';
+$lang['de_DE']['DataObjectManager']['SAVE'] = 'Speichern';
+$lang['de_DE']['DataObjectManager']['SAVEANDADD'] = 'Speichere und neu: %s';
+$lang['de_DE']['DataObjectManager']['SAVED'] = '%s erfolgreich gespeichert';
+$lang['de_DE']['DataObjectManager']['SEARCH'] = 'Suchen';
+$lang['de_DE']['DataObjectManager']['SELECTFOLDER'] = 'Wählen Sie einen Ordner';
+$lang['de_DE']['DataObjectManager']['SILVERSTRIPEVERSION'] = 'DataObjectManager benötigt Silverstripe 2.3 oder höher.';
+$lang['de_DE']['DataObjectManager']['TO'] = 'bis';
+$lang['de_DE']['DataObjectManager']['TOTALNOT100'] = 'Spaltenbreite muss Total 100 ergeben und nicht %s';
+$lang['de_DE']['DataObjectManager']['UPLOADFROMPC'] = 'Dateien von Ihrem Computer';
+$lang['de_DE']['DataObjectManager']['VIEWFIRST'] = 'Zeige Erste';
+$lang['de_DE']['DataObjectManager']['VIEWLAST'] = 'Zeige Letzte';
+$lang['de_DE']['DataObjectManager']['VIEWNEXT'] = 'Zeige Nächste';
+$lang['de_DE']['DataObjectManager']['VIEWPREVIOUS'] = 'Zeige Vorherige';
+$lang['de_DE']['DataObjectManager_popup.ss']['NEXT'] = 'Weiter';
+$lang['de_DE']['DataObjectManager_popup.ss']['PREVIOUS'] = 'Zurück';
+$lang['de_DE']['FileDataObjectManager']['GRIDVIEW'] = 'Miniaturansicht';
+$lang['de_DE']['FileDataObjectManager']['LISTVIEW'] = 'Listenansicht';
+$lang['de_DE']['FileDataObjectManager']['SWFUPLOAD'] = 'DataObjectManager benötigt das SWFUpload Modul.';
+?>

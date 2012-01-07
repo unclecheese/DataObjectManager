ff34bbf23e754a2950c93d3b10efeeca38fe92fa
Juan Molina <juan.molina@club.fr>
2012-01-07 17:16:53 +0100

    Added French_France translation

---
+++ lang/fr_FR.php
@@ -0,0 +1,82 @@
+<?php
+
+global $lang;
+
+$lang['fr_FR']['AssetManager']['ADDFILESTO'] = 'fichiers à « %s »';
+$lang['fr_FR']['AssetManager']['FILES'] = 'fichiers';
+$lang['fr_FR']['DataObjectManager']['ADD'] = 'Ajouter';
+$lang['fr_FR']['DataObjectManager']['ADDEDNEW'] = '%s ajouté avec succès';
+$lang['fr_FR']['DataObjectManager']['ADDITEM'] = array(
+	'Ajouter %s',
+	PR_MEDIUM,
+	'Add [name]'
+);
+$lang['fr_FR']['DataObjectManager']['ALL'] = 'Tout';
+$lang['fr_FR']['DataObjectManager']['AND'] = 'et';
+$lang['fr_FR']['DataObjectManager']['ANDOTHERCOMPONENTS'] = 'et %d autres composants';
+$lang['fr_FR']['DataObjectManager']['CONFIRMDELETE'] = 'Supprimer ?';
+$lang['fr_FR']['DataObjectManager']['COPIESOFOBJECT'] = ' copies de ce %s';
+$lang['fr_FR']['DataObjectManager']['COULDNOTFINDRELATION'] = 'Je n&rsquo;ai pas pu trouver une rélation d&rsquo;appartenance pour la classe %s';
+$lang['fr_FR']['DataObjectManager']['CREATE'] = 'Créer ';
+$lang['fr_FR']['DataObjectManager']['DELETE'] = 'Supprimer';
+$lang['fr_FR']['DataObjectManager']['DESELECTALL'] = 'décocher tout';
+$lang['fr_FR']['DataObjectManager']['DISPLAYING'] = 'Affichage :';
+$lang['fr_FR']['DataObjectManager']['DRAGDROP'] = 'Ordonner par glisser-déposer';
+$lang['fr_FR']['DataObjectManager']['DUPLICATE'] = 'Dupliquer';
+$lang['fr_FR']['DataObjectManager']['DUPLICATESCREATED'] = 'duplication(s) crées';
+$lang['fr_FR']['DataObjectManager']['EDIT'] = 'Éditer';
+$lang['fr_FR']['DataObjectManager']['ERROR'] = 'Erreur';
+$lang['fr_FR']['DataObjectManager']['ERRORDUPLICATING'] = 'Une erreur s&rsquo;est produite pendant la duplication.';
+$lang['fr_FR']['DataObjectManager']['IMPORTFROMFOLDER'] = 'Importer d&rsquo;un dossier existant';
+$lang['fr_FR']['DataObjectManager']['INCLUDERELATIONS'] = 'Inclure les objets rattachés';
+$lang['fr_FR']['DataObjectManager']['NOFILTER'] = 'Sans filtre';
+$lang['fr_FR']['DataObjectManager']['NOITEMSFOUND'] = 'Aucun %s trouvé';
+$lang['fr_FR']['DataObjectManager']['OF'] = 'de';
+$lang['fr_FR']['DataObjectManager']['ONLYRELATED'] = 'Afficher les registres rattachés';
+$lang['fr_FR']['DataObjectManager']['PERPAGERESULTS'] = 'résultats par page';
+$lang['fr_FR']['DataObjectManager']['PERPAGESHOW'] = 'Afficher';
+$lang['fr_FR']['DataObjectManager']['REFRESH'] = 'rafraîchir';
+$lang['fr_FR']['DataObjectManager']['SAVE'] = 'Enregistrer';
+$lang['fr_FR']['DataObjectManager']['SAVEANDADD'] = 'Enregistrer et ajouter %s';
+$lang['fr_FR']['DataObjectManager']['SAVED'] = '%s enregistré avec succès';
+$lang['fr_FR']['DataObjectManager']['SEARCH'] = 'Rechercher';
+$lang['fr_FR']['DataObjectManager']['SELECTFOLDER'] = 'Choisir un dossier';
+$lang['fr_FR']['DataObjectManager']['SILVERSTRIPEVERSION'] = 'DataObjectManager requiert Silverstripe 2.3 ou supérieur.';
+$lang['fr_FR']['DataObjectManager']['TO'] = 'à';
+$lang['fr_FR']['DataObjectManager']['TOTALNOT100'] = 'La largeur des colonnes doit faire 100 au total, et non %s';
+$lang['fr_FR']['DataObjectManager']['UPLOADFROMPC'] = 'Télécharger depuis mon ordinateur';
+$lang['fr_FR']['DataObjectManager']['VIEW'] = 'Voir';
+$lang['fr_FR']['DataObjectManager']['VIEWFIRST'] = 'Voir le premier';
+$lang['fr_FR']['DataObjectManager']['VIEWLAST'] = 'Voir le dernier';
+$lang['fr_FR']['DataObjectManager']['VIEWNEXT'] = 'Voir le suivant';
+$lang['fr_FR']['DataObjectManager']['VIEWPREVIOUS'] = 'Voir le précédent';
+$lang['fr_FR']['DataObjectManager']['WITHRELATIONS'] = 'en incluant les rélations';
+$lang['fr_FR']['DataObjectManager_popup.ss']['NEXT'] = 'Suivant';
+$lang['fr_FR']['DataObjectManager_popup.ss']['PREVIOUS'] = 'Précédent';
+$lang['fr_FR']['FLV']['PLURALNAME'] = array(
+	'Fichiers',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['fr_FR']['FLV']['SINGULARNAME'] = array(
+	'Fichier',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['fr_FR']['FileDataObjectManager']['FILERELATION'] = 'Je n&rsquo;ai pas pu déterminer la relation avec le fichier';
+$lang['fr_FR']['FileDataObjectManager']['GRIDVIEW'] = 'Vue en grille';
+$lang['fr_FR']['FileDataObjectManager']['LISTVIEW'] = 'Vue en liste';
+$lang['fr_FR']['FileDataObjectManager']['SWFUPLOADUPLOADIFY'] = 'DataObjectManager requiert les modules Uploadify ou SWFUpload.';
+$lang['fr_FR']['LiveDropdownField']['BROWSE'] = 'Parcourir…';
+$lang['fr_FR']['MP3']['PLURALNAME'] = array(
+	'Fichiers',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['fr_FR']['MP3']['SINGULARNAME'] = array(
+	'Fichier',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+
+?>

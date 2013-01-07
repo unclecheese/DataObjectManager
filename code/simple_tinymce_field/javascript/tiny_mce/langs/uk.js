0979a4735f1aa2e17fe554d6724e4d1f33d65a7d
unknown <C:\Users\Eugene\AppData\Roaming\The Bat! Pwd>
2013-01-07 13:54:29 +0200

    - Fixed many_many sortable issues: table field wrong order in code, added   an ability to maintain multiple relations to the same class. + Ukrainian localisation added. + Main Russian localisation added.

---
+++ code/simple_tinymce_field/javascript/tiny_mce/langs/uk.js
@@ -0,0 +1,154 @@
+﻿tinyMCE.addI18n({uk:{
+common:{
+edit_confirm:"Чи бажаэте ви використати WYSIWYG-редактор для цього поля?",
+apply:"Застосувати",
+insert:"Вставити",
+update:"Поновити",
+cancel:"Відмінити",
+close:"Зактити",
+browse:"Обрати",
+class_name:"Клас",
+not_set:"-- не встановлено --",
+clipboard_msg:"Копіювання/Вирізка/Вставка не доступні в Mozilla та Firefox.\nЧи бажаєте узнати більше про цю проблему?",
+clipboard_no_support:"Наразі не підтримується вашим браузером. Використовуйте клавіатурні комбінації для цієї дії.",
+popup_blocked:"Прикро, але ваш блокувальник popup-вікон заблокував вікно, яке реалізує функціонал редактора. Вам необхідно відключити блокування для цього сайту, щоб мати змогу користуватися цим інструментом.",
+invalid_data:"Помилка: Введено некоретні дані. Такі поля позначені червоним.",
+more_colors:"Ще кольори"
+},
+contextmenu:{
+align:"Вирівнювання",
+left:"По лівому краю",
+center:"По центру",
+right:"По правому краю",
+full:"По ширині"
+},
+insertdatetime:{
+date_fmt:"%Y-%m-%d",
+time_fmt:"%H:%M:%S",
+insertdate_desc:"Вставити дату",
+inserttime_desc:"Вставити час",
+months_long:"Січень,Лютий,Березень,Квітень,Травень,Червень,Липень,Серпень,Вересень,Жовтень,Листопад,Грудень",
+months_short:"Січ,Лют,Бер,Квт,Трв,Чрв,Лип,Сер,Вер,Жов,Лис,Грд",
+day_long:"Неділя,Понеділок,Вівторок,Середа,Червер,П'ятниця,Субота,Неділя",
+day_short:"Нд,Пн,Вт,Ср,Чт,Пт,Сб,Нд"
+},
+print:{
+print_desc:"Друкувати"
+},
+preview:{
+preview_desc:"Попередній перегляд"
+},
+directionality:{
+ltr_desc:"Напрям зліва направо",
+rtl_desc:"Напрям зправа наліво"
+},
+layer:{
+insertlayer_desc:"Додати новий шар",
+forward_desc:"Вперед",
+backward_desc:"Назад",
+absolute_desc:"Перемкнути абсолютне позиціювання",
+content:"Новий шар..."
+},
+save:{
+save_desc:"Зберегти",
+cancel_desc:"Відмінити всі зміни"
+},
+nonbreaking:{
+nonbreaking_desc:"Вставити нерозривний пробіл"
+},
+iespell:{
+iespell_desc:"Перевірити орфографію",
+download:"ieSpell не знайдено. Чи бажаєте встановити?"
+},
+advhr:{
+advhr_desc:"Horizontal rule"
+},
+emotions:{
+emotions_desc:"Смайлики"
+},
+searchreplace:{
+search_desc:"Шукати",
+replace_desc:"Пошук/Заміна"
+},
+advimage:{
+image_desc:"Вставити/редагувати зображення"
+},
+advlink:{
+link_desc:"Вставити/редагувати посилання"
+},
+xhtmlxtras:{
+cite_desc:"Цитування",
+abbr_desc:"Абревіатура",
+acronym_desc:"Акронім",
+del_desc:"Видалити",
+ins_desc:"Вставити",
+attribs_desc:"Вставити/редагувати атрибути"
+},
+style:{
+desc:"Редагувати CSS Стиль"
+},
+paste:{
+paste_text_desc:"Вставити як текст",
+paste_word_desc:"Вставити з Word",
+selectall_desc:"Виділити все"
+},
+paste_dlg:{
+text_title:"Користуйтесь CTRL+V на клавіатурі щоб вставити текст у вікно.",
+text_linebreaks:"Зберігати переноси рядків",
+word_title:"Користуйтесь CTRL+V на клавіатурі щоб вставити текст у вікно."
+},
+table:{
+desc:"Вставити нову таблицю",
+row_before_desc:"Додати рядок перед",
+row_after_desc:"Додати рядок після",
+delete_row_desc:"Видалити рядок",
+col_before_desc:"Додати стовбчик перед",
+col_after_desc:"Додати стовбчик після",
+delete_col_desc:"Видалити стовбчик",
+split_cells_desc:"Розділити об'єднані комірки таблиці",
+merge_cells_desc:"Об'єднати комірки",
+row_desc:"Властивості рядка таблиці",
+cell_desc:"Властивості комірки таблиці",
+props_desc:"Властивості таблиці",
+paste_row_before_desc:"Вставити рядок перед",
+paste_row_after_desc:"Вставити рядок після",
+cut_row_desc:"Вирізати рядок",
+copy_row_desc:"Копіювати рядок",
+del:"Видалити таблицю",
+row:"Рядок",
+col:"Стовбчик",
+cell:"Комірка"
+},
+autosave:{
+unload_msg:"Якщо ви залишите сторінку, ві зміни, що ви не зберегли, буде втрачено."
+},
+fullscreen:{
+desc:"Перемкнути повноекранний режим"
+},
+media:{
+desc:"Встатити/редагувати embedded media",
+edit:"Редагувати embedded media"
+},
+fullpage:{
+desc:"Властивості документу"
+},
+template:{
+desc:"Вставити predefined template content"
+},
+visualchars:{
+desc:"Показувати/ні службові символи."
+},
+spellchecker:{
+desc:"Увімкнути/вимкнути перевірку орфографії",
+menu:"Налаштування перевірки орфографії",
+ignore_word:"Ігнорувати слово",
+ignore_words:"Ігнорувати все",
+langs:"Мови",
+wait:"Будь ласка, зачекайте...",
+sug:"Підказки",
+no_sug:"Немає підказок",
+no_mpell:"Помилок не знайдено."
+},
+pagebreak:{
+desc:"Вставити розрив сторінки."
+}}});
---
+++ lang/uk_UA.php
@@ -0,0 +1,68 @@
+<?php
+
+global $lang;
+
+$lang['uk_UA']['AssetManager']['ADDFILESTO'] = 'файли до "%s"';
+$lang['uk_UA']['AssetManager']['FILES'] = 'файли';
+$lang['uk_UA']['DataObjectManager']['ADD'] = 'Додати';
+$lang['uk_UA']['DataObjectManager']['ADDEDNEW'] = 'Створення %s успішно завершено';
+$lang['uk_UA']['DataObjectManager']['ADDITEM'] = array(
+	'Додати %s',
+	PR_MEDIUM,
+	'Add [name]'
+);
+$lang['uk_UA']['DataObjectManager']['ALL'] = 'Всі';
+$lang['uk_UA']['DataObjectManager']['AND'] = 'та';
+$lang['uk_UA']['DataObjectManager']['ANDOTHERCOMPONENTS'] = 'та %d інших компонентів';
+$lang['uk_UA']['DataObjectManager']['ALLOWEDFILETYPES'] = 'Дозволені лише файли типу %s';
+$lang['uk_UA']['DataObjectManager']['CONFIRMDELETE'] = 'Видатили?';
+$lang['uk_UA']['DataObjectManager']['COPIESOFOBJECT'] = ' копій %s';
+$lang['uk_UA']['DataObjectManager']['COULDNOTFINDRELATION'] = 'Не вдається знайти відношення для класу %s';
+$lang['uk_UA']['DataObjectManager']['CREATE'] = 'Створити ';
+$lang['uk_UA']['DataObjectManager']['DELETE'] = 'Видалити';
+$lang['uk_UA']['DataObjectManager']['DESELECTALL'] = 'зняти виділення';
+$lang['uk_UA']['DataObjectManager']['DISPLAYING'] = 'Перегляд від';
+$lang['uk_UA']['DataObjectManager']['DRAGDROP'] = 'Дозволити перетягування';
+$lang['uk_UA']['DataObjectManager']['DUPLICATE'] = 'Клонувати';
+$lang['uk_UA']['DataObjectManager']['DUPLICATESCREATED'] = 'клонування виконано';
+$lang['uk_UA']['DataObjectManager']['EDIT'] = 'Редагувати';
+$lang['uk_UA']['DataObjectManager']['ERROR'] = 'Помилка';
+$lang['uk_UA']['DataObjectManager']['ERRORDUPLICATING'] = 'Під час клонування виникла помилка.';
+$lang['uk_UA']['DataObjectManager']['EXPORT'] = 'Експортувати %s';
+$lang['uk_UA']['DataObjectManager']['IMPORTFROMFOLDER'] = 'Імпортувати з існуючої папки';
+$lang['uk_UA']['DataObjectManager']['INCLUDERELATIONS'] = 'Разом зі зв\'язаними об\'єктами';
+$lang['uk_UA']['DataObjectManager']['NOFILTER'] = 'Без фільтра';
+$lang['uk_UA']['DataObjectManager']['NOITEMSFOUND'] = '%s не знайдено';
+$lang['uk_UA']['DataObjectManager']['OF'] = 'з';
+$lang['uk_UA']['DataObjectManager']['ONLYRELATED'] = 'Лише пов\'язані об\'єкти';
+$lang['uk_UA']['DataObjectManager']['PERPAGERESULTS'] = 'результатів на сторінку';
+$lang['uk_UA']['DataObjectManager']['PERPAGESHOW'] = 'Показати';
+$lang['uk_UA']['DataObjectManager']['REFRESH'] = 'обновити';
+$lang['uk_UA']['DataObjectManager']['SAVE'] = 'Зберегти';
+$lang['uk_UA']['DataObjectManager']['SAVEANDADD'] = 'Зберегти та додати %s';
+$lang['uk_UA']['DataObjectManager']['SAVED'] = '%s успішно збережено';
+$lang['uk_UA']['DataObjectManager']['SEARCH'] = 'Шукати';
+$lang['uk_UA']['DataObjectManager']['SELECTFOLDER'] = 'Виберіть папку';
+$lang['uk_UA']['DataObjectManager']['SILVERSTRIPEVERSION'] = 'DataObjectManager потребує Silverstripe версії 2.3 або вище.';
+$lang['uk_UA']['DataObjectManager']['TO'] = 'до';
+$lang['uk_UA']['DataObjectManager']['TOTALNOT100'] = 'Ширини колонок в сумі мають складати 100, а не %s';
+$lang['uk_UA']['DataObjectManager']['UPLOADFROMPC'] = 'Завантажити з мого комп\'ютера';
+$lang['uk_UA']['DataObjectManager']['VIEW'] = 'Переглянути';
+$lang['uk_UA']['DataObjectManager']['VIEWFIRST'] = 'Перші';
+$lang['uk_UA']['DataObjectManager']['VIEWLAST'] = 'Останні';
+$lang['uk_UA']['DataObjectManager']['VIEWNEXT'] = 'Наступні';
+$lang['uk_UA']['DataObjectManager']['VIEWPREVIOUS'] = 'Попередні';
+$lang['uk_UA']['DataObjectManager']['WITHRELATIONS'] = 'разом з відношеннями';
+$lang['uk_UA']['DataObjectManager_popup.ss']['NEXT'] = 'Далі';
+$lang['uk_UA']['DataObjectManager_popup.ss']['PREVIOUS'] = 'Назад';
+$lang['uk_UA']['FLV']['PLURALNAME'] = 'Відеозаписи (FLV)';
+$lang['uk_UA']['FLV']['SINGULARNAME'] = 'Відеозапис (FLV)';
+$lang['uk_UA']['FileDataObjectManager']['FILERELATION'] = 'Не можу визначити зв\'язок файлу';
+$lang['uk_UA']['FileDataObjectManager']['GRIDVIEW'] = 'У вигляді сітки';
+$lang['uk_UA']['FileDataObjectManager']['LISTVIEW'] = 'У вигляді списку';
+$lang['uk_UA']['FileDataObjectManager']['SWFUPLOAD'] = 'DataObjectManager потребує модуль Uploadify.';
+$lang['uk_UA']['FileDataObjectManager']['SWFUPLOADUPLOADIFY'] = 'DataObjectManager потребує модуль Uploadify.';
+$lang['uk_UA']['LiveDropdownField']['BROWSE'] = 'Вибрати…';
+$lang['uk_UA']['MP3']['PLURALNAME'] = 'Звукозаписи (MP3)';
+$lang['uk_UA']['MP3']['SINGULARNAME'] = 'Звукозапис (MP3)';
+
